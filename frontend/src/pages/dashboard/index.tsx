5f3f5e74698f0a66868a49636336d63055428f73
n.bobard <n.bobard@DPK-142>
2019-08-17 13:44:07 +0430

    pages folder added

---
+++ frontend/src/pages/dashboard/index.tsx
@@ -0,0 +1,71 @@
+import * as React from "react";
+import Navigation from "../../components/navigation";
+import { debug } from "util";
+import PersonList from '../../components/personList';
+import List from '../../components/list';
+import { Panel, PanelType, DefaultButton } from "office-ui-fabric-react";
+import "./dashboard.css";
+/**
+ * Dashboard
+ */
+interface Props {
+    showPanel: false
+}
+
+/**
+ * Demo List
+ */
+class Dashboard extends React.Component<Props, any> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = { showPanel: false };
+    }
+
+    public render() {
+        return (
+            <div>
+                
+                <DefaultButton text="Open panel" onClick={this._setShowPanel(true)} />
+                <Panel
+                    isBlocking={false}
+                    isOpen={this.state.showPanel}
+                    onDismiss={this._setShowPanel(false)}
+                    type={PanelType.smallFixedFar}
+                    headerText="Non-Modal Panel"
+                    hasCloseButton={false}
+                    isFooterAtBottom={true}
+                    onRenderFooterContent={this._onRenderFooterContent}
+                    >
+                    <Navigation 
+                        actions={[]} 
+                        showDialog={false}
+
+                         />
+                </Panel>
+                <PersonList />
+            </div >
+        );
+    }
+
+    private _setShowPanel = (showPanel: boolean): (() => void) => {
+        return (): void => {
+            this.setState({ showPanel });
+        };
+    };
+
+    private _onClosePanel = () => {
+        this.setState({ showPanel: false });
+      };
+
+    private _onRenderFooterContent = (): JSX.Element => {
+        return (
+          <div>
+            <DefaultButton onClick={this._onClosePanel}>Cancel</DefaultButton>
+          </div>
+        );
+      };
+
+}
+
+export default Dashboard;
